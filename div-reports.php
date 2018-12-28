db331ad12b6cbf088b47ca7c3e29fd0603fa4641
compuvin <compuvin@users.noreply.github.com>
2018-12-28 12:34:45 -0500

    Added Limit and Counter
    Added Limit and Counter to the list of reports. This way the link to the reports list can be changed when the limit approaches assuming there are more reports listed than can be replaced.

--- div-reports.php
+++ div-reports.php
@@ -1,17 +1,27 @@
 <?php
 //Common Reports - Referred by index.php
 
+//Variables
+$LimitR = 20; //Changable limit of reports to display
+$i = 0; //counter
+
 //get results from database
-$result = mysqli_query($connection,"SELECT ID, ReportName FROM customreports order by SortOrder limit 20");
+$result = mysqli_query($connection,"SELECT ID, ReportName FROM customreports order by SortOrder limit $LimitR");
 $all_property = array();  //declare an array for saving property
 
 echo "<ul class=\"bullet\">\r\n\t";
 	while ($row = mysqli_fetch_array($result)) {
         echo "<li><a href=\"customreports.php?id=" . $row["ID"] . "\">" . $row["ReportName"] . "</a></li>\r\n\t"; //Make the report name clickable
+		$i++; //increase counter
 	}
 echo "</ul>";
 echo "<br>";
-echo "<a href=\"reportslist.php\">View More / Edit List</a>";
+if ($i < $LimitR)
+{
+	echo "<center><a href=\"reportslist.php\">Edit List</a></center>";
+} else {
+	echo "<center><a href=\"reportslist.php\">View More</a></center>";
+}
 ?>
 <!--	<ul class="bullet">
 		<li><a href="discoveredapps.php">All Unique</a></li>
